914bbe9e7ed19e08b0138499e48b89266bc906a9
Jamyan <148853505+Jamyan-1234@users.noreply.github.com>
2024-10-31 21:07:33 +0800

    test

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import HomePage from "./pages/HomePage";
 import RegisterPage from "./pages/RegisterPage";
 
+<<<<<<< HEAD
 function App() {
     return (
         <>
@@ -9,6 +10,23 @@ function App() {
             <RegisterPage />
         </>
     );
+=======
+
+//-----------------------------------------------------------Үндсэн нүүр хуудас-------------------------------------------
+
+//Энэ хэсэгт заал захиалагч анх нэвтэрч орох үед харагдах хэсгийг кодыг бичнэ
+
+
+
+function Undsen_Huudas() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> 1 + 1 = 11</h1>
+      </header>
+    </div>
+  );
+>>>>>>> dd66d74 (test)
 }
 
 export default App;
