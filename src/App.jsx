61acc6025b0810646828148aa08e1e4627875b7d
Jamyan <148853505+Jamyan-1234@users.noreply.github.com>
2024-10-31 21:33:40 +0800

    gg

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,6 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import { useNavigate } from 'react-router-dom';
 import './App.css';
-
-function Undsen_Huudas() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <h1> qqqqqqqqqqqqqqqq</h1>
-            </header>
-        </div>
-    );
-  }
-
-export default Undsen_Huudas;
-=======
-import "./App.css";
 import HomePage from "./pages/HomePage";
 import RegisterPage from "./pages/RegisterPage";
-
-<<<<<<< HEAD
 function App() {
     return (
         <>
@@ -27,29 +8,6 @@ function App() {
             <RegisterPage />
         </>
     );
-=======
-
-//-----------------------------------------------------------Үндсэн нүүр хуудас-------------------------------------------
-
-//Энэ хэсэгт заал захиалагч анх нэвтэрч орох үед харагдах хэсгийг кодыг бичнэ
-
-
-
-function Undsen_Huudas() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <button onclick="redirect()">
-          Redirect to GFG
-        </button>
-
-
-        
-      </header>
-    </div>
-  );
->>>>>>> dd66d74 (test)
-}
+  }
 
 export default App;
->>>>>>> a019f3d (test)
