dd66d74a52c8701d12c789412843e1004d2e50a1
Jamyan <148853505+Jamyan-1234@users.noreply.github.com>
2024-10-31 21:07:33 +0800

    test

--- src/App.js
+++ src/App.js
@@ -12,18 +12,7 @@ function Undsen_Huudas() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> HOOLONDOOOOO
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1> 1 + 1 = 11</h1>
       </header>
     </div>
   );
