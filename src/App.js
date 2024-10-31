6743913d6295e79c6d54d076f297194b08df51d9
Jamyan <148853505+Jamyan-1234@users.noreply.github.com>
2024-10-31 21:14:37 +0800

    t

--- src/App.js
+++ src/App.js
@@ -1,32 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
 
 //-----------------------------------------------------------Үндсэн нүүр хуудас-------------------------------------------
 
 //Энэ хэсэгт заал захиалагч анх нэвтэрч орох үед харагдах хэсгийг кодыг бичнэ
 
-
-
 function Undsen_Huudas() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> HOOLONDOOOOO
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1> qqqqqqqqqqqqqqqq</h1>
+            </header>
+        </div>
+    );
 }
 
 export default Undsen_Huudas;
