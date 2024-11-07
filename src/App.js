f8cdb315f3eb70c3adf3a7c168cb6524fe883c4e
B210930022 <benhhslen@gmail.com>
2024-11-07 18:15:39 +0800

    edit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Navbar from "./components/Navbar";
+import MasterLayout from "./layout/MasterLayout";
 import HomePage from "./pages/HomePage";
 import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
     return (
-        <>
-            <Navbar />
-            <HomePage />
-            <RegisterPage />
-        </>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<MasterLayout />}>
+                    <Route index element={<HomePage />} />
+                    <Route path="/register" element={<RegisterPage />} />
+                    <Route path="/login" element={<LoginPage />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
     );
 }
 
---
+++ src/layout/MasterLayout.js
@@ -0,0 +1,18 @@
+import { Outlet } from "react-router-dom";
+import Header from "./Header";
+import Footer from "./Footer";
+
+const MasterLayout = () => {
+    return (
+        <>
+            <Header />
+            <main className="py-6 px-4 sm:p-6 md:py-10 md:px-8">
+                <div className="w-full"></div>
+            </main>
+            <Outlet />
+            <Footer />
+        </>
+    );
+};
+
+export default MasterLayout;
