46395552f6ec6bf634b7e6c4d1bb350c34a36446
B210930022 <benhhslen@gmail.com>
2024-11-14 17:36:21 +0800

    comment ustgaw

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -2,7 +2,6 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Button from "../components/Button";
-//hehehehehehehehehehehhe----------------------------------------------------------------------------------------------------------
 const RegisterPage = () => {
     const [username, setUsername] = useState("");
     const [email, setEmail] = useState("");
