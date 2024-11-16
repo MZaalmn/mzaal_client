46bb04f484ef07392079228ae7c8e0f826957600
Jamyan <148853505+Jamyan-1234@users.noreply.github.com>
2024-11-16 19:24:18 +0800

    test

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
 import ForgotPassword from "./pages/ForgotPassword";
 
+
 function App() {
     return (
         <BrowserRouter>
@@ -18,6 +19,8 @@ function App() {
                         path="/forgot-password"
                         element={<ForgotPassword />}
                     />
+
+                    
                 </Route>
             </Routes>
         </BrowserRouter>
