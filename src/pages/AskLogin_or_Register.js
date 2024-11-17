93c1206da3fb9b9fcbfdc75419f48acb5e4bd55b
Jamyan <148853505+Jamyan-1234@users.noreply.github.com>
2024-11-17 20:23:10 +0800

    Хэрэглэгч аль эрхээр нэвтэрч орох эсэхийг асуух хэсэг оруулав

--- src/pages/AskLogin_or_Register.js
+++ src/pages/AskLogin_or_Register.js
@@ -1,9 +1,11 @@
 // src/pages/AskLogin_or_Register.js
 import React from "react";
+import '../Css_files/AskLoginOrRegister.css';
 
 const AskLoginOrRegister = () => {
     return (
-        <div>
+        
+        <div className="body">
             <h1>TEST---------------------------------------</h1>
         </div>
     );
